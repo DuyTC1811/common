bd75696c04e3377cd9ec135711cfcdcf201ec6aa
Duy.T.C <duytran.81811@gmail.com>
2023-01-30 21:36:42 +0700

    create: common exceptions

---
+++ exceptions/src/main/java/io/exceptions/AggregateNotFoundException.java
@@ -0,0 +1,8 @@
+package io.exceptions;
+
+public class AggregateNotFoundException extends RuntimeException {
+    public AggregateNotFoundException(String message) {
+        super(message);
+    }
+
+}
